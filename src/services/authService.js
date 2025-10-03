e59cc47a542f7a87138c3b7e2b9d814ed2936fd6
Mathías López <mathiaslopez.tech@outlook.com>
2025-10-02 23:30:06 -0300

    feat: the user is not allowed to log in until validated

--- src/services/authService.js
+++ src/services/authService.js
@@ -8,6 +8,10 @@ class AuthService {
       return { success: false, message: 'Invalid credentials' };
     }
 
+    if (!user.isActive) {
+      return { success: false, message: 'You must validate your email address before you can log in.' };
+    }
+
     if (await UserService.checkPassword({ user: { password: user.password }, password })) {
       const token = jwt.sign({ sub: "1" }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN, algorithm: process.env.JWT_ALGORITHM });
       return { success: true, token };
